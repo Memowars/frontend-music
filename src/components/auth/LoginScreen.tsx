631bff36eebea37456324bf46fbfa775b970ffef
Memowars <guillermo.villarreal@enroutesystems.com~>
2022-05-08 21:33:06 -0500

    validations on login form

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -1,71 +1,83 @@
 import { Button } from '@mui/material';
 import Link from '@mui/material/Link';
-
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import React from 'react';
+import { IForm } from '../../Interfaces/IForm';
 export const LoginScreen = () => {
-  /*   const [formLoginValues, handleLoginInputChange] = useForm({
-    loginEmail: 'memowars@outlook.es',
-    loginPassword: '123456',
-  }); */
-
-  const handleLogin = (e: any) => {
-    e.preventDefault();
-    console.log('login');
-  };
-  const handleLoginInputChange = (e: any) => {
-    e.preventDefault();
-    console.log('xd');
-  };
+  const formik = useFormik<IForm>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Correo invalido').required('Correo requerido'),
+      password: Yup.string().required('Contraseña requerida'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="loging_Container ">
       <hr
         style={{ color: 'white', width: '700px' }}
         className="animate__animated  animate__backInLeft"
       />
-      <h1 className="animate__animated animate__fadeIn">Inicio de sesión</h1>
-      <div>
-        <Button style={{ textTransform: 'none' }} color="inherit">
-          <p style={{ marginRight: '3px' }}>¿Aún no tienes una cuenta? </p>
-          <Link href="/">Registrate</Link>
-        </Button>
-      </div>
-      <div className="login-form">
-        <form action="post">
-          <div className="Login_input_container">
-            <p>Correo electronico</p>
-            <input
-              type="text"
-              className="Login_input"
-              placeholder="Correo"
-              name="loginEmail"
-              onChange={handleLoginInputChange}
-            />
-          </div>
-          <div className="Login_input_container">
-            <p>Contraseña</p>
-            <input
-              type="password"
-              className="Login_input"
-              placeholder="Contraseña"
-              name="loginEmail"
-              onChange={handleLoginInputChange}
-            />
-          </div>
-          <div className="Login_button">
-            <Button
-              color="primary"
-              type="submit"
-              value="Iniciar sesión"
-              variant="outlined"
-              size="large"
-              onClick={handleLogin}
-            >
-              Iniciar sesión
-            </Button>
-          </div>
-          <div>
-            <p>¿Olvidaste tu contraseña?</p>
-          </div>
-        </form>
+      <div className="animate__animated animate__fadeIn animate__delay-1s">
+        <h1 className="">Inicio de sesión</h1>
+        <div>
+          <Button style={{ textTransform: 'none' }} color="inherit">
+            <p style={{ marginRight: '3px' }}>¿Aún no tienes una cuenta? </p>
+            <Link href="/">Registrate</Link>
+          </Button>
+        </div>
+        <div className="login-form">
+          <form action="post" onSubmit={formik.handleSubmit}>
+            <div className="Login_input_container">
+              {formik.touched.email && formik.errors.email ? (
+                <p>{formik.errors.email}</p>
+              ) : null}
+              <input
+                type="text"
+                className="Login_input"
+                placeholder="Correo"
+                name="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleChange}
+                value={formik.values.email}
+              />
+            </div>
+            <div className="Login_input_container">
+              {formik.touched.password && formik.errors.password ? (
+                <p>{formik.errors.password}</p>
+              ) : null}
+              <input
+                type="password"
+                className="Login_input"
+                placeholder="Contraseña"
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleChange}
+                value={formik.values.password}
+              />
+            </div>
+            <div className="Login_button">
+              <Button
+                color="primary"
+                type="submit"
+                value="Iniciar sesión"
+                variant="outlined"
+                size="large"
+              >
+                Iniciar sesión
+              </Button>
+            </div>
+            <div>
+              <p>¿Olvidaste tu contraseña?</p>
+            </div>
+          </form>
+        </div>
       </div>
 
       <hr
